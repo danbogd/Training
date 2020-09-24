b68e06005926535f67271c623c7599c9b259b155
Denis Bogdanov <36646499+danbogd@users.noreply.github.com>
2020-09-24 11:24:26 +0300

    Create test.sol

---
+++ test.sol
@@ -0,0 +1,8 @@
+   function transferFrom(address from, address to, uint tokens) external override returns (bool success) {
+        require(to != address(0));//  require(sender != address(0), "ERC20: transfer from the zero address");
+        balances[from] = safeSub(balances[from], tokens);
+        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(from, to, tokens);
+        return true;
+   }
